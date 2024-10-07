69e88642603e8ee50e6672c36083ce8972a1afb2
Bits-Hunter <109984246+Rick-hunterr@users.noreply.github.com>
2024-10-07 07:25:19 -0300

    agregue las opciones del menu

--- Masterclass/script.js
+++ Masterclass/script.js
@@ -1,8 +1,17 @@
 window.addEventListener("scroll",function(){
     var header = document.querySelector("header");
     header.classList.toggle("abajo",window.scrollY>0)
-})
+});
 window.addEventListener("scroll", function(){
     var Comentar = document.querySelector(".Comentario");
     Comentar.classList.toggle("Comenton", window.scrollY > 2400)
-})
+});
+function mostrar() {
+  const settings = document.querySelector('.Settings');
+  if (settings.style.display === 'flex') {
+    settings.style.display = 'none';
+  } else {
+    settings.style.display = 'flex';
+  }
+}
+
