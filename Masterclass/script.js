17a6c0a1cb6eacebcd0c619403df78210506e160
Bits-Hunter <109984246+Rick-hunterr@users.noreply.github.com>
2024-10-07 01:37:21 -0300

    1.5

--- Masterclass/script.js
+++ Masterclass/script.js
@@ -2,7 +2,7 @@ window.addEventListener("scroll",function(){
     var header = document.querySelector("header");
     header.classList.toggle("abajo",window.scrollY>0)
 })
-//window.addEventListener("scroll", function(){
-  //  var Comentar = document.querySelector(".Comentario");
-    //Comentar.classList.toggle("Comenton", window.scrollY > 2400)
-//})
+window.addEventListener("scroll", function(){
+    var Comentar = document.querySelector(".Comentario");
+    Comentar.classList.toggle("Comenton", window.scrollY > 2400)
+})
